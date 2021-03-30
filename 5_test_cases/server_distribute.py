e99fea0ea358c4bea47f84ce55f9616e4bd8fd91
Vu Dinh Dong <64526817+Dongvd01680@users.noreply.github.com>
2021-03-30 16:03:22 +0700

    Update

--- 5_test_cases/server_distribute.py
+++ 5_test_cases/server_distribute.py
@@ -2,58 +2,46 @@ import  csv
 import mysql.connector
 import math
 
-# Get list form Database
+# Variable
+global limit, offset
+table = "creditscore"
+
+
+# Connect to Database
 mydb = mysql.connector.connect(
   host="10.83.35.70",
   user="root",
   password="123$%^",
-  database="rule_management"
+  database="credit_score"
 )
-mycursor = mydb.cursor()
-mycursor.execute("SELECT count(*) FROM data_model")
-myresult = mycursor.fetchall()
 
-for x in myresult:
-  
-  print(x)
+# Select and count number of User
+mycursor = mydb.cursor()
+mycursor.execute(f"SELECT * FROM {table}")
+myresult = mycursor.fetchall()
+users = 0
+for data in myresult:
+  users+=1
+print('Number of Users = ',users)
+
 
 # Read CSV File
 with open('server.csv', 'r') as csvFile:
     reader = csv.reader(csvFile)
     server = []
+    ips=0
     for row in reader:
         server.append(row)
+        ips+=1
+    print('Number of Server IPs = ',ips)    
+    limit = math.ceil(users/ips)
 
-# Select DB by index
-    for x in server:
-        index = server.index(x)
-        # print('Index of', x ,'is',index)
-        if index == 0:
-          mycursor.execute("SELECT id FROM data_model LIMIT 5 OFFSET 0")
-          server1 = mycursor.fetchall()
-          for x in myresult:
-            print('list 1',server1)
-        elif  index == 1:
-          mycursor.execute("SELECT id FROM data_model LIMIT 5 OFFSET 5")
-          server2 = mycursor.fetchall()
-          for x in myresult:
-            print('list 2',server2)
-        elif  index == 2:
-          mycursor.execute("SELECT id FROM data_model LIMIT 5 OFFSET 10")
-          server3 = mycursor.fetchall()
-          for x in myresult:
-            print('list 3',server3)
-        elif  index == 3:
-          mycursor.execute("SELECT id FROM data_model LIMIT 5 OFFSET 15")
-          server4 = mycursor.fetchall()
-          for x in myresult:
-            print('list 4',server4)
-        else:
-          mycursor.execute("SELECT id FROM data_model LIMIT 5 OFFSET 20")
-          server5 = mycursor.fetchall()
-          for x in myresult:
-            print('list 5',server5)
-
-    
-
-
+ # Query DB with Limit and Offset
+ 
+for x in server:
+  index = server.index(x)
+  offset = index*limit
+  mycursor.execute(f"SELECT id FROM {table} LIMIT {limit} OFFSET {offset}")
+  server1 = mycursor.fetchall()
+  print(f'ID List {index}',server1)
+       
